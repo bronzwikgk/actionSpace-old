0b2801afc3fe582c020c819f5df62883ab63734e
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-16 12:14:37 +0530

    u

---
+++ form/actionSpaceModelV1.js
@@ -0,0 +1,16 @@
+var actionSpaceModel={
+    editor:{
+        name:'div',
+        id: 'actionSpaceEditor',
+        class: 'container',
+        insertMenu: 'insertMenu',
+        contextMenu:'contextMenu',
+        blocks: 'actionBlocks',
+        footer: 'actionEditorFooter',
+        
+        
+
+
+    },
+
+}
