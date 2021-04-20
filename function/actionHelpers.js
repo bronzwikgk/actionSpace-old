030d902159e4fd11a02cbe27e7af281dba0fdee5
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-20 13:42:05 +0530

    u

--- function/actionHelpers.js
+++ function/actionHelpers.js
@@ -157,18 +157,3 @@ function groupBy(arr, criteria) {
 
     }, {});
 }
-
-let cache = new Map();
-
-function loadCached(url) {
-    if (cache.has(url)) {
-        return Promise.resolve(cache.get(url)); // (*)
-    }
-
-    return fetch(url)
-        .then(response => response.text())
-        .then(text => {
-            cache.set(url, text);
-            return text;
-        });
-}
