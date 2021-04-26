da1ec3b501a1f51c552c50fa5c06f2baa9249685
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-26 15:40:50 +0530

    u

---
+++ form/viewcomponenets.js
@@ -0,0 +1,21 @@
+var viewCore = {
+    Core
+This only contains the essential styles needed to build a project with Cirrus.It consists of these components:
+
+        Buttons
+Code
+Default(Base)
+Font
+Footer
+Forms
+Frames
+Header
+Layout
+Links
+Lists
+Media
+Modal
+Tables
+Theme
+Util
+}
