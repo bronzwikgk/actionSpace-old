eb7c799502166781c945cff4320a1a83298f05ed
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-28 18:45:06 +0530

    u

--- function/actionSpaceEditorV1.js
+++ function/actionSpaceEditorV1.js
@@ -1,12 +1,14 @@
 /**
- * This  class creates an editor, that allows users to 
+ * This  class converts a normal HTML element
  */
 class ActionSpaceEditor{
     constructor(element,elementContent,storageResources,config){
-this._editor = element,
-this._editorContentInjson = elementContent,
-this._storageResources = storageResources,
-this._config = config
+        this._editor = element,
+        this._editor.contentEditable = 'true';
+//console.log(this._editor),
+        this._editorContentInjson = elementContent,
+        this._storageResources = storageResources,
+        this._config = config
 
     }
     getCaretNode(){
@@ -15,7 +17,22 @@ this._config = config
     getCaretIndexPosition(){
     }
     getCaretXyPosition(){
-
+        let x = 0,
+        y = 0;
+    const isSupported = typeof window.getSelection !== "undefined";
+    if (isSupported) {
+        const selection = window.getSelection();
+        if (selection.rangeCount !== 0) {
+            const range = selection.getRangeAt(0).cloneRange();
+            range.collapse(true);
+            const rect = range.getClientRects()[0];
+            if (rect) {
+                x = rect.left;
+                y = rect.top;
+            }
+        }
+    }
+    return { x, y };
     }
     getTextBeforeCaret (){
 
@@ -23,8 +40,29 @@ this._config = config
     getTextAfterCaret(){
 
     }
+    moveCaret(win, charCount) {
+        var sel, range;
+        if (win.getSelection) {
+            // IE9+ and other browsers
+            sel = win.getSelection();
+            if (sel.rangeCount > 0) {
+                var textNode = sel.focusNode;
+                var newOffset = sel.focusOffset + charCount;
+                sel.collapse(textNode, Math.min(textNode.length, newOffset));
+            }
+        } else if ((sel = win.document.selection)) {
+            // IE <= 8
+            if (sel.type != "Control") {
+                range = sel.createRange();
+                range.move("character", charCount);
+                range.select();
+            }
+        }
+    }
+
+  
 
 }
 
-var ActionSpaceEditorInstance = new ActionSpaceEditor(window[ActionSpaceEditor]);
-
+var actionSpaceEditorInstance = new ActionSpaceEditor(window['actionSpaceEditor']);
+console.log(actionSpaceEditorInstance);
