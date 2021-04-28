0f9183401ace679d2699d814b411d60a952b14b9
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-28 18:34:16 +0530

    u

---
+++ function/actionSpaceEditorV1.js
@@ -0,0 +1,30 @@
+/**
+ * This  class creates an editor, that allows users to 
+ */
+class ActionSpaceEditor{
+    constructor(element,elementContent,storageResources,config){
+this._editor = element,
+this._editorContentInjson = elementContent,
+this._storageResources = storageResources,
+this._config = config
+
+    }
+    getCaretNode(){
+
+    }
+    getCaretIndexPosition(){
+    }
+    getCaretXyPosition(){
+
+    }
+    getTextBeforeCaret (){
+
+    }
+    getTextAfterCaret(){
+
+    }
+
+}
+
+var ActionSpaceEditorInstance = new ActionSpaceEditor(window[ActionSpaceEditor]);
+
