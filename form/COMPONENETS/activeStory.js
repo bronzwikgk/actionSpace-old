09c6b40f9fcf993faddce507b5101e892c49003b
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-29 20:07:16 +0530

    u

---
+++ form/COMPONENETS/activeStory.js
@@ -0,0 +1,8 @@
+
+var activeStoryTree = {
+    name: 'span',
+    class: 'sticky verticle ',
+    id: 'activeStoryTree',
+    textContent: 'activeStoryTree',
+    innerHTML: 'create Tree Element'
+}
--- function/test.js
+++ function/test.js
@@ -5,3 +5,7 @@ const isMobile = /Android|BlackBerry|iPad|iPod|iPhone|webOS/i
     .test(navigator.userAgent);
 const touchSupported = 'ontouchstart' in window || 
     (window.DocumentTouch && document instanceof DocumentTouch);
+
+var platform = typeof browser === 'undefined'
+  ? chrome
+  : browser
