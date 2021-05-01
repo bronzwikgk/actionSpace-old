41ec815505ba3a3da906128982a784841ed3e1a5
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-05-01 13:15:09 +0530

    u

--- v2/form/viewComponents/actionSpaceView.js
+++ v2/form/viewComponents/actionSpaceView.js
@@ -1,3 +1,22 @@
 var activeActionSpace = {
-    
+    header: {
+        name: 'div',
+        class: 'row container',
+        id:'header',
+        
+    },
+    body: {
+        sidebar: {
+            
+        },
+        actionSpaceBody: {},
+        rightBar:{},
+
+    },
+    footer: {
+        name: 'div',
+        class: 'container row',
+        id:'footer',
+    }
+
 }
