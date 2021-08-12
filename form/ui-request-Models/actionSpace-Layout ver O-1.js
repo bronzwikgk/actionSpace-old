aecbc611dcc6ff0bf89ae4787e6611a81b19e994
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-08-12 18:25:51 +0530

    u

---
+++ form/ui-request-Models/actionSpace-Layout ver O-1.js
@@ -0,0 +1,33 @@
+var actionSpace_Layout_VerO_1 = {
+header:{
+    brand:{
+
+    },
+    search:{
+
+    },
+    nav_menu: {
+        action:{},
+        people:{},
+        setting:{},
+        help:{},
+        profile:{}
+
+    },
+
+},
+body:{
+    left_side_bar:{
+
+    },
+    playground:{
+        editor:{
+
+        }
+    }
+
+}
+
+
+
+}
